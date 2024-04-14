2302ac1123499624f1cb544e5094d6e5a39ce7c7
inko <77970966+inko16@users.noreply.github.com>
2024-04-15 00:02:43 +0800

    json filter

--- yamljson.cfworker.js
+++ yamljson.cfworker.js
@@ -11,13 +11,40 @@ export default {
     if(request.url.includes(prefix)){
       let url = request.url;
       let yamlurl = url.slice(url.indexOf(prefix)+prefix.length);
+      console.log(yamlurl);
       if(!yamlurl.startsWith("http"))
         yamlurl = "https://"+yamlurl;
       let dest = new URL(yamlurl);
+      console.log(dest);
       let yamlbody = await fetch(dest).then(r=>r.text());
+      // extract singbox{...}
+      const regex = /singbox{([^}]*)}/;
+      const matches = yamlbody.match(regex);
+      console.log(matches);
+
       let jsondest = yaml.load(yamlbody);
+
+      if (matches) {
+        // return json as singbox{...} specified
+        let sing_obj = matches[1].replace(/ /g,"").split(',');
+        console.log(sing_obj);
+        let jsonout = {};
+        for(let val of sing_obj.values()){
+          jsonout[val] = jsondest[val];
+        }
+        console.log(jsonout);
+        return Response.json(jsonout);
+        
+      }
       return Response.json(jsondest);
     }
+    let str =
+`inko:
+  - ab: cd
+    ef: gh
+`;
+    const jsond = yaml.load(str);
+    console.log(jsond);
     return new Response('Hello World!');
   },
 };
